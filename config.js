b121c8662fda71436b18f26b981ea623e83568e4
haideptraii <87479229+haideptraii@users.noreply.github.com>
2021-07-15 19:34:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 const CONFIG = {
-    introTitle: 'Emn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Emmm à!',
+    introDesc: `Chuyện là nhà anh bao việc
+    Mà anh vẫn cứ nhiệt liệt yêu em.
+    Nhớ em anh tỉnh bằng trà, yêu em anh trả bằng tình được không ? 
+    btnIntro: 'Nhấn vào đây tiếp nhé 😁',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích Hảii <33',
+    btnNo: 'Không, Hải đừng có mơ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh 🙄 <3',
+    btnReply: 'Ấn đây tiếp nè...:',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
